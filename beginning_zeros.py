45a6b85d413759cf98c3543218939b59a5309f95
Шелохин Игорь <uropb2013@yandex.ru>
2021-11-29 16:34:19 +0300

    beginning_zeros complete

---
+++ beginning_zeros.py
@@ -0,0 +1,20 @@
+def beginning_zeros(number: str) -> int:
+    # your code here
+    num = 0
+    while num != len(number) and number[num] == '0':
+        num += 1
+    return num
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(beginning_zeros('100'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert beginning_zeros('100') == 0
+    assert beginning_zeros('001') == 2
+    assert beginning_zeros('100100') == 0
+    assert beginning_zeros('001001') == 2
+    assert beginning_zeros('012345679') == 1
+    assert beginning_zeros('0000') == 4
+    print("Coding complete? Click 'Check' to earn cool rewards!")
