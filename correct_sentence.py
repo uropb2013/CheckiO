05829c6032ae821869c0c09d01f50c7e70643c3f
Шелохин Игорь <uropb2013@yandex.ru>
2021-11-29 19:07:26 +0300

    correct_sentence complete

---
+++ correct_sentence.py
@@ -0,0 +1,20 @@
+def correct_sentence(text: str) -> str:
+    # your code here
+    a = text[0].upper() + text[1:]
+    if a[-1] == ".":
+        return a
+    return a + "."
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(correct_sentence("greetings, friends"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert correct_sentence("greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends.") == "Greetings, friends."
+    assert correct_sentence("hi") == "Hi."
+    assert correct_sentence("welcome to New York") == "Welcome to New York."
+
+    print("Coding complete? Click 'Check' to earn cool rewards!")
